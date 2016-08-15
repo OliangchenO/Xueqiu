7e9175cdf57c22849aaea55d3a0cf4400e2243c2
Rockyzsu <jinweizsu@gmail.com>
2016-08-15 09:36:24 +0800

    update

--- main_project/main_project/spiders/xueqiu.py
+++ main_project/main_project/spiders/xueqiu.py
@@ -1,19 +1,13 @@
+#-*-coding=utf-8-*-
+__author__ = 'rocky'
 import scrapy
 
+class XueQiuSpider(scrapy.Spider):
 
-class MySpider(scrapy.spiders.Spider):
     name = "xueqiu"
     allowed_domains = ["xueqiu.com"]
     start_urls = ["http://xueqiu.com/8255849716"]
 
-
-'''
-name="30daydo"
-allowed_domains=["30daydo.com"]
-start_urls=["http://30daydo.com"]
-'''
-
-
-def parse(self, response):
-    print "*" * 10
-    print response.body
+    def parse(self, response):
+        print "*" * 10
+        print response.url
